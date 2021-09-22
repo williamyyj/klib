a33bc75eb6e70348c4b1433b383878cc2b57ab02
William Yin <williamyyj@gmail.com>
2021-09-23 00:32:38 +0800

    tomcat jdbc pool

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.5.10"
     java
+    `maven-publish`
 }
 
-group = "org.example"
+group = "org.cc"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,7 +13,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
